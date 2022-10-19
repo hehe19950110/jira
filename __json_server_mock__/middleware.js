3f3c1ad398d46f7a8ad5bb3efa933e5288962099
ChrisWen <chriswen96.com>
2022-10-19 15:19:15 +0800

    fix: 路径修改

--- __json_server_mock__/middleware.js
+++ __json_server_mock__/middleware.js
@@ -1,6 +1,5 @@
 module.exports = (req, res, next) => {
-  console.log(req.path);
-  if (req.method === "POST" && req.path === "./login") {
+  if (req.method === "POST" && req.path === "/login") {
     if (req.body.username === "jack" && req.body.password === "123456") {
       return res.status(200).json({
         user: {
